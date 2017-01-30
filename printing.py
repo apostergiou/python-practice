3b35e130aaa713f3a6e409b8d0a8da20ead2ad7b
Apostolis Stergiou <apostergiou@gmail.com>
2017-01-30 07:36:33 +0200

    Use a formmater

--- printing.py
+++ printing.py
@@ -6,3 +6,12 @@ sum2 = 20
 print sum1 + sum2,
 print sum1
 print sum2
+
+formatter = "%r"
+print formatter % (1)
+print formatter % (true)
+print formatter % (formatter)
+print formatter % ('one')
+print formatter % (
+    "Hello world!"
+)
