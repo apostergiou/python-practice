94448a1572f73566a0693e647bf037d99224122b
Apostolis Stergiou <apostergiou@gmail.com>
2017-01-30 07:37:30 +0200

    Move printing practice in a single file

--- printing-variables.py
+++
@@ -1,8 +0,0 @@
-my_town = "Trikala"
-population = 700
-
-print "My town is %s." % my_town
-print "My town's population is %d." % population
-
-print "My town is %s and it has a population of %d." % (
-    my_town, population)
