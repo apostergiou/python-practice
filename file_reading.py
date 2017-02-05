0a5605a53bb9f4215624dce86120592873f8104b
Apostolis Stergiou <apostergiou@gmail.com>
2017-02-05 14:02:06 +0200

    Practice with file writing

--- file_reading.py
+++ file_reading.py
@@ -2,7 +2,19 @@ from sys import argv
 
 script, file = argv
 
-text = open(file).read()
+target_file = open(file, 'w')
 
 print "The contents of the file %r are:" % file
-print text
+print target_file
+
+print "Truncating the file."
+target_file.truncate()
+
+print "Give me some input:"
+input = raw_input("|> ")
+
+print "Let's write it to the file."
+target_file.write(input)
+
+print "We are finished, so we will close the file."
+target_file.close()
